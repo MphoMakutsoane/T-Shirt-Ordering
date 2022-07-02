e2f0acff8324886e780f52a6f158d8a25bd97cb9
Admin <tshedison929@gmail.com>
2022-07-02 14:14:35 +0200

    Add project files.

---
+++ Swag App/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Swag_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+
+            var page = new NavigationPage(new SwaggPage());
+            page.BarTextColor = Color.Aquamarine;
+            page.BarBackgroundColor = Color.Transparent;
+            //(Color)App.Current.Resources["primaryGreen"];
+            MainPage = page;
+        }
+
+
+//        MainPage = new MainPage();
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
